41b4b8d88a516b174fce994ba21cd6af4e67d5c6
ArtemSadovenko <asadovenko7@gmail.com>
2024-04-17 22:10:45 +0300

    Footer commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,6 @@
 import React from "react";
 import { Link, useParams } from "react-router-dom";
 
-// interface Props {
-//     nextPage: () => void,
-//     prevPage: () => void
-// }
-
 type FooterInfo = {
     id: Number
 }
@@ -14,20 +9,8 @@ const maxPage = 42;
 const minPage = 1;
 
 export default function Footer(_props:FooterInfo){
-
-    // const params = useParams();
-
-    // let id: Number;
-    // if( params.id == null){
-    //     id = 1;
-    // }else{
-    //     id = Number(params.id);
-    // }
-    
     return(
         <div>
-            {/* <button onClick={prevPage}>`{'<'}`</button>
-            <button onClick={nextPage}>`{'>'}`</button> */}
             <Link to={`/page/${_props.id == minPage ? minPage: Number(_props.id)- 1}`}>PREV</Link>
             <Link to={`/page/${_props.id == maxPage ? maxPage: Number(_props.id)+ 1}`}>NEXT</Link>
 
